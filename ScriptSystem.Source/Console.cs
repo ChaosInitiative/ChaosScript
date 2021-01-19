a55a17f40621c03c654e933486f1f29a672fff01
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2021-01-19 05:02:12 -0800

    feat: Some initial work on script bindings. Added the ChaosScriptNativeBindings.dll

---
+++ ScriptSystem.Source/Console.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ScriptSystem.Source;
+
+namespace ScriptSystem.Source
+{
+    public class Console
+    {
+        public void Msg(String msg)
+        {
+            Logging.Msg(msg);
+        }
+
+        public void DevMsg(String msg)
+        {
+            Logging.DevMsg(msg);
+        }
+
+        public void Warning(String msg)
+        {
+            Logging.Warning(msg);
+        }
+
+        public void DevWarning(String msg)
+        {
+            Logging.DevWarning(msg);
+        }
+
+        public void MsgC(Color color, String message)
+        {
+            Logging.ConColorMsg(color, message);
+        }
+    }
+}
---
+++ ScriptSystem.Source/LogChannel.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Runtime.InteropServices;
+using ScriptSystem.Source;
+
+namespace ScriptSystem.Source
+{
+    public class LogChannel
+    {
+        private Logging.LogSeverity _ChannelSeverity { set; get; }
+        private String _ChannelName { set; get; }
+        private Color _ChannelColor { set; get; }
+        private SWIGTYPE_p_LoggingChannelID_t ChannelId;
+        
+        public LogChannel(String channelName, Logging.LogSeverity severity, Color color)
+        {
+            _ChannelName = channelName;
+            _ChannelSeverity = severity;
+            _ChannelColor = color;
+
+            ChannelId = Logging.RegisterLoggingChannel(channelName, severity, color);
+        }
+
+        public void Log(String msg, Logging.LogSeverity severity = Logging.LogSeverity.LS_MESSAGE)
+        {
+            Logging.Log(ChannelId, severity, msg);
+        }
+
+        public void Log(Color color, String msg, Logging.LogSeverity severity = Logging.LogSeverity.LS_MESSAGE)
+        {
+            Logging.Log(ChannelId, severity, color, msg);
+        }
+
+        public void LogWarning(String msg)
+        {
+            Logging.Log(ChannelId, Logging.LogSeverity.LS_WARNING, msg);
+        }
+
+        public void LogWarning(Color color, String msg)
+        {
+            Logging.Log(ChannelId, Logging.LogSeverity.LS_WARNING, color, msg);
+        }
+
+        public void SetChannelColor(Color color)
+        {
+            Logging.SetChannelColor(ChannelId, (uint)color.GetRawColor());
+            _ChannelColor = color;
+        }
+
+        public Color GetChannelColor()
+        {
+            return _ChannelColor;
+        }
+
+        public String GetChannelName()
+        {
+            return _ChannelName;
+        }
+
+        public Logging.LogSeverity GetChannelSeverity()
+        {
+            return _ChannelSeverity;
+        }
+
+        public void SetChannelSeverity(Logging.LogSeverity severity)
+        {
+            _ChannelSeverity = severity;
+            Logging.SetLoggingLevel(ChannelId, severity);
+        }
+    }
+}
