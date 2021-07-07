3b6711a8a18c1cd641ca0a775f99c126119c2fd1
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2021-07-06 18:17:18 -0700

    chore: Remove old file

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ "$(uname)" = "Linux" ]]; then  
-    dotnet build --configuration Debug -o ../game/bin/managed/linux64
-else
-    dotnet build --configuration Debug -o ../game/bin/managed/win64
-fi 
